11aae95fd4f55d0a59e368c25241c869d40105f0
Bogdangonch <bogdan.goncharenko95@gmail.com>
2023-04-09 17:26:48 +0200

    Homework#2

--- src/components/auth/register/RegisterPage.tsx
+++ src/components/auth/register/RegisterPage.tsx
@@ -4,52 +4,71 @@ import { IRegisterPage } from "./types";
 const RegisterPage = () => {
   //створили конкретни екземлеяр на основі нашого інтерфейсу
   const init: IRegisterPage = {
-    username: "",
+    firstname: "",
+    lastname: "",
     email: "",
     password: "",
+    confirmPassword: "",
+    phone: "",
+    image: "",
   };
 
   //При зміни значення елемента в useState компонент рендериться повторно і виводить нові значення
   const [data, setData] = useState<IRegisterPage>(init);
 
-  console.log("Render Register component", "------SALO----");
+  //   console.log("Render Register component", "------SALO----");
 
   //console.log("Дестурктуризація", {...data, password: "123456"});
 
   const onSubmitHandler = (e: any) => {
     e.preventDefault();
     console.log("Ми відправляємо на сервер", data);
+    setData(init);
     //setData({email: "pylyp", password: "123456"});
   };
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log("Щось вводити в інтпут");
-    console.log(e.target.name, e.target.value);
+    // console.log("Щось вводити в інтпут");
+    // console.log(e.target.name, e.target.value);
     setData({ ...data, [e.target.name]: e.target.value });
+    // onClear(e.target);
   };
   return (
     <>
       <h1 className="text-center">Реєстрація на сайті</h1>
       <form onSubmit={onSubmitHandler} className="col-md-6 offset-md-3">
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Ваше ім'я
+          <label htmlFor="firstname" className="form-label">
+            First Name
           </label>
           <input
             type="text"
             className="form-control"
-            id="username"
-            name="username"
-            value={data.username}
+            id="firstname"
+            name="firstname"
             onChange={onChangeHandler}
+            value={data.firstname}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastname" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastname"
+            name="lastname"
+            onChange={onChangeHandler}
+            value={data.lastname}
           />
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Електронна адреса
+            Email
           </label>
           <input
-            type="text"
+            type="email"
             className="form-control"
             id="email"
             name="email"
@@ -60,7 +79,7 @@ const RegisterPage = () => {
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
-            Пароль
+            Password
           </label>
           <input
             type="password"
@@ -69,6 +88,46 @@ const RegisterPage = () => {
             name="password"
             onChange={onChangeHandler}
             value={data.password}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            name="confirmPassword"
+            onChange={onChangeHandler}
+            value={data.confirmPassword}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phone" className="form-label">
+            Phone
+          </label>
+          <input
+            type="tel"
+            className="form-control"
+            id="phone"
+            name="phone"
+            onChange={onChangeHandler}
+            value={data.phone}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phone" className="form-label">
+            Image
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            className="form-control"
+            id="image"
+            name="image"
+            onChange={onChangeHandler}
+            value={data.image}
           />
         </div>
 
