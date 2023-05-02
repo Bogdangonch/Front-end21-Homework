160de69719d9fa2942a383e46a1861d2e4fa47f0
Bogdangonch <bogdan.goncharenko95@gmail.com>
2023-05-02 20:29:26 +0200

    Homework#6

---
+++ src/components/auth/register/types.ts
@@ -0,0 +1,26 @@
+export interface IRegisterPage {
+    email: string;
+    firstName: string;
+    secondName: string;
+    photo: string;
+    phone: string;
+    password: string;
+    confirmPassword: string;
+    // image: File|null;
+    // countryId: number;
+  }
+
+  export interface IRegisterError {
+    email: string[];
+    firstName: string[];
+    secondName: string[];
+    phone: string[];
+    photo: string[];
+    password: string[];
+    confirmPassword: string[];
+  }
+
+  export interface ISelectItem {
+    id: number;
+    name: string;
+  }
